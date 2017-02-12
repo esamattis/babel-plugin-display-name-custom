8bfcf13f799871aafd7b9ff07c1bf83ae7b1af2b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-12 17:24:47 +0000

    Handle scopes

--- babel-plugin/plugin.js
+++ babel-plugin/plugin.js
@@ -1,30 +1,47 @@
 module.exports = function (babel) {
     const {types: t} = babel;
-    var componentCreatorName = null;
+    var componentCreatorNames = null;
 
     return {
         visitor: {
-            Program(path) {
-                componentCreatorName = null;
+            Program() {
+                componentCreatorNames = null;
             },
 
             ImportDeclaration(path) {
                 if (path.node.source.value !== "react-simple") return;
-                if (!componentCreatorName) {
-                    componentCreatorName = {};
+                if (!componentCreatorNames) {
+                    componentCreatorNames = {};
                 }
 
-
-                componentCreatorName[path.node.specifiers[0].local.name] = true;
+                componentCreatorNames[path.node.specifiers[0].local.name] = true;
 
             },
 
             VariableDeclaration(path) {
-                if (!componentCreatorName) return;
+                if (!componentCreatorNames) return;
 
                 if (!path.node.declarations[0].init) return;
                 if (path.node.declarations[0].init.type !== "CallExpression") return;
-                if (!componentCreatorName[path.node.declarations[0].init.callee.name]) return;
+
+                const creatorName = path.node.declarations[0].init.callee.name;
+
+                if (!componentCreatorNames[creatorName]) return;
+
+                var scope = path.scope;
+
+                var usingImportedCreator = false;
+
+                while (scope) {
+                    if (scope.bindings[creatorName]) {
+                        usingImportedCreator = scope.bindings[creatorName].kind === "module";
+                        break;
+                    }
+                    scope = scope.parent;
+                }
+
+                if (!usingImportedCreator) return;
+
 
                 var name = path.node.declarations[0].id.name;
 
