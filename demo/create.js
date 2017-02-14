26450b2929e40a4ebf55416cc51710fea972ce0a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-14 18:28:47 +0000

    cleanup

--- demo/create.js
+++ demo/create.js
@@ -3,5 +3,3 @@ import React from "react";
 export const createComponent = (Component, styles) => {
     return props => <Component {...props} style={styles} />;
 };
-
-export default createComponent;
